7e77859efc565d085e8855c2c099d3bb88782beb
Arron-B <arronmail@yahoo.com>
2024-11-02 15:27:43 +0000

    linking to github repo

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ BANDS = [
 async def bands() -> list[dict, dict, dict, dict]:
     return BANDS
 
-@app.get('/bands/{band_id}')
+@app.get('/bands/{band_id}', status_code = 206)
 async def band(band_id: int) -> dict:
     band = next((b for b in BANDS if b['id'] == band_id), None)
     if band is None:
         # status code 404
         raise HTTPException(status_code=404, detail='Band not found')
     return band
+
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: str) -> list[dict]:
+    return [
+        b for b in BANDS if b['genre'].lower() == genre.lower()
+    ]
