7c95f45e5f8ab17d6b7c511f107d0222a3900a26
Arron-B <arronmail@yahoo.com>
2024-11-02 17:30:15 +0000

    lesson 3 complete

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 from fastapi import FastAPI, HTTPException
+from schemas import GenreURLChoices, Band
 
 app = FastAPI()
+
 
 BANDS = [
     {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
     {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
-    {'id': 3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal', 'albums': [
+        {'title': 'Master of Reality', 'release_date': '1971-07-21'}
+    ]},
     {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
 ]
 
 @app.get('/bands') 
-async def bands() -> list[dict, dict, dict, dict]:
-    return BANDS
+async def bands() -> list[Band]:
+    return [
+        Band(**b) for b in BANDS # ** is like the spread operator (...) in JS. But can only be used on a dictionary. * is used for lists.
+    ]
 
 @app.get('/bands/{band_id}', status_code = 206)
-async def band(band_id: int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         # status code 404
         raise HTTPException(status_code=404, detail='Band not found')
     return band
 
 @app.get('/bands/genre/{genre}')
-async def bands_for_genre(genre: str) -> list[dict]:
+async def bands_for_genre(genre: GenreURLChoices) -> list[dict]: #parameter type hints use : and return type hints use ->
+    print(genre.value)
     return [
-        b for b in BANDS if b['genre'].lower() == genre.lower()
+        b for b in BANDS if b['genre'].lower() == genre.value #genre.value retrieves the string value of the selected genre Enum member
     ]
